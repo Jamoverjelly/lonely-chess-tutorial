72aa06151c3da6cc7fb1b84ab7a31a7c8f81b607
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-03-27 17:19:25 -0400

    knight, squares, and board have all been successfully rendered to the page. Initialized concept of changing state in app with a Game component which updates the render and randomly re-positions the knight.

---
+++ src/Board.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Square from "./Square";
+import Knight from "./Knight";
+
+/* Styling properties applied to the board element */
+const boardStyle = {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexWrap: "wrap"
+};
+
+/* Styling properties applied to each square element */
+const squareStyle = {
+  width: "12.5%",
+  height: "12.5%"
+};
+
+function renderSquare(i, [knightX, knightY]) {
+  const x = i % 8;
+  const y = Math.floor(i / 8);
+  const isKnightHere = x === knightX && y === knightY;
+  const black = (x + y) % 2 === 1;
+  const piece = isKnightHere ? <Knight /> : null;
+
+  return (
+    <div key={i} style={squareStyle}>
+      <Square black={black}>{piece}</Square>
+    </div>
+  );
+}
+
+export default function Board({ knightPosition }) {
+  const squares = [];
+  for (let i = 0; i < 64; i++) {
+    squares.push(renderSquare(i, knightPosition));
+  }
+
+  return <div style={boardStyle}>{squares}</div>;
+}
